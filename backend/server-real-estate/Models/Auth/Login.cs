d0864297e450b7d67d391e1b83d89135df0ec67a
Adeayo <aadelee74@gmail.com>
2025-03-20 13:38:56 +0000

    Add .gitignore to exclude build artifacts and user-specific files; simplify Login model

--- backend/server-real-estate/Models/Auth/Login.cs
+++ backend/server-real-estate/Models/Auth/Login.cs
@@ -1,9 +1,7 @@
 namespace server_real_estate.Model;
 public class Login
 {
-
     public required string Email { get; set; }
     public required string Password { get; set; }
-
     public bool? RememberMe { get; set; }
 }
